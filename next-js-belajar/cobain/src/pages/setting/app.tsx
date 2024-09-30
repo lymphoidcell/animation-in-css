a2dabb29091936abfc8db5612740481867d1e434
Phanie <101379066+lymphoidcell@users.noreply.github.com>
2024-09-30 17:17:06 +0700

    Delete next-js-belajar/cobain directory

--- next-js-belajar/cobain/src/pages/setting/app.tsx
+++
@@ -1,9 +0,0 @@
-const AppSettingPage = () => {
-    return (
-        <div>
-            <h1>App Setting</h1>
-        </div>
-    );
-};
-
-export default AppSettingPage;
--- next-js-belajar/cobain/src/pages/setting/user.tsx
+++
@@ -1,9 +0,0 @@
-const UserSettingPage = () => {
-  return (
-    <div>
-      <h1>User Setting</h1>
-    </div>
-  );
-};
-
-export default UserSettingPage;
